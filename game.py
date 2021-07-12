03a10696308e20e28107a5597d183011d9775e58
shoemjoh <86920229+shoemjoh@users.noreply.github.com>
2021-07-12 19:28:58 -0400

    Capture user input

---
+++ game.py
@@ -0,0 +1,15 @@
+print("Rock, Paper, Scissors, Shoot!")
+
+
+# ASK for a user input
+# source: https://docs.python.org/3/library/functions
+x = input("Please choose one of either 'rock', 'paper', or scissors': ")
+print(x)
+# At this point in time, we have tested and confirmed this section, let's commit it.
+# Validate the user input
+
+
+# Generate a computer choice
+
+
+# Determine a winner and display results
